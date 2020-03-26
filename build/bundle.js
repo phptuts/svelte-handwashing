483198cfdeea85b9ad3bbb0d7d83886107bcb5c6
Noah Glaser <glaserpower@gmail.com>
2020-03-26 13:03:46 -0700

    Updates

---
+++ build/bundle.js
@@ -0,0 +1,621 @@
+var app = (function () {
+    'use strict';
+
+    function noop() { }
+    function run(fn) {
+        return fn();
+    }
+    function blank_object() {
+        return Object.create(null);
+    }
+    function run_all(fns) {
+        fns.forEach(run);
+    }
+    function is_function(thing) {
+        return typeof thing === 'function';
+    }
+    function safe_not_equal(a, b) {
+        return a != a ? b == b : a !== b || ((a && typeof a === 'object') || typeof a === 'function');
+    }
+
+    function append(target, node) {
+        target.appendChild(node);
+    }
+    function insert(target, node, anchor) {
+        target.insertBefore(node, anchor || null);
+    }
+    function detach(node) {
+        node.parentNode.removeChild(node);
+    }
+    function element(name) {
+        return document.createElement(name);
+    }
+    function text(data) {
+        return document.createTextNode(data);
+    }
+    function space() {
+        return text(' ');
+    }
+    function listen(node, event, handler, options) {
+        node.addEventListener(event, handler, options);
+        return () => node.removeEventListener(event, handler, options);
+    }
+    function attr(node, attribute, value) {
+        if (value == null)
+            node.removeAttribute(attribute);
+        else if (node.getAttribute(attribute) !== value)
+            node.setAttribute(attribute, value);
+    }
+    function children(element) {
+        return Array.from(element.childNodes);
+    }
+    function set_data(text, data) {
+        data = '' + data;
+        if (text.data !== data)
+            text.data = data;
+    }
+    function set_style(node, key, value, important) {
+        node.style.setProperty(key, value, important ? 'important' : '');
+    }
+    function custom_event(type, detail) {
+        const e = document.createEvent('CustomEvent');
+        e.initCustomEvent(type, false, false, detail);
+        return e;
+    }
+
+    let current_component;
+    function set_current_component(component) {
+        current_component = component;
+    }
+    function get_current_component() {
+        if (!current_component)
+            throw new Error(`Function called outside component initialization`);
+        return current_component;
+    }
+    function createEventDispatcher() {
+        const component = get_current_component();
+        return (type, detail) => {
+            const callbacks = component.$$.callbacks[type];
+            if (callbacks) {
+                // TODO are there situations where events could be dispatched
+                // in a server (non-DOM) environment?
+                const event = custom_event(type, detail);
+                callbacks.slice().forEach(fn => {
+                    fn.call(component, event);
+                });
+            }
+        };
+    }
+
+    const dirty_components = [];
+    const binding_callbacks = [];
+    const render_callbacks = [];
+    const flush_callbacks = [];
+    const resolved_promise = Promise.resolve();
+    let update_scheduled = false;
+    function schedule_update() {
+        if (!update_scheduled) {
+            update_scheduled = true;
+            resolved_promise.then(flush);
+        }
+    }
+    function add_render_callback(fn) {
+        render_callbacks.push(fn);
+    }
+    let flushing = false;
+    const seen_callbacks = new Set();
+    function flush() {
+        if (flushing)
+            return;
+        flushing = true;
+        do {
+            // first, call beforeUpdate functions
+            // and update components
+            for (let i = 0; i < dirty_components.length; i += 1) {
+                const component = dirty_components[i];
+                set_current_component(component);
+                update(component.$$);
+            }
+            dirty_components.length = 0;
+            while (binding_callbacks.length)
+                binding_callbacks.pop()();
+            // then, once components are updated, call
+            // afterUpdate functions. This may cause
+            // subsequent updates...
+            for (let i = 0; i < render_callbacks.length; i += 1) {
+                const callback = render_callbacks[i];
+                if (!seen_callbacks.has(callback)) {
+                    // ...so guard against infinite loops
+                    seen_callbacks.add(callback);
+                    callback();
+                }
+            }
+            render_callbacks.length = 0;
+        } while (dirty_components.length);
+        while (flush_callbacks.length) {
+            flush_callbacks.pop()();
+        }
+        update_scheduled = false;
+        flushing = false;
+        seen_callbacks.clear();
+    }
+    function update($$) {
+        if ($$.fragment !== null) {
+            $$.update();
+            run_all($$.before_update);
+            const dirty = $$.dirty;
+            $$.dirty = [-1];
+            $$.fragment && $$.fragment.p($$.ctx, dirty);
+            $$.after_update.forEach(add_render_callback);
+        }
+    }
+    const outroing = new Set();
+    let outros;
+    function transition_in(block, local) {
+        if (block && block.i) {
+            outroing.delete(block);
+            block.i(local);
+        }
+    }
+    function transition_out(block, local, detach, callback) {
+        if (block && block.o) {
+            if (outroing.has(block))
+                return;
+            outroing.add(block);
+            outros.c.push(() => {
+                outroing.delete(block);
+                if (callback) {
+                    if (detach)
+                        block.d(1);
+                    callback();
+                }
+            });
+            block.o(local);
+        }
+    }
+    function create_component(block) {
+        block && block.c();
+    }
+    function mount_component(component, target, anchor) {
+        const { fragment, on_mount, on_destroy, after_update } = component.$$;
+        fragment && fragment.m(target, anchor);
+        // onMount happens before the initial afterUpdate
+        add_render_callback(() => {
+            const new_on_destroy = on_mount.map(run).filter(is_function);
+            if (on_destroy) {
+                on_destroy.push(...new_on_destroy);
+            }
+            else {
+                // Edge case - component was destroyed immediately,
+                // most likely as a result of a binding initialising
+                run_all(new_on_destroy);
+            }
+            component.$$.on_mount = [];
+        });
+        after_update.forEach(add_render_callback);
+    }
+    function destroy_component(component, detaching) {
+        const $$ = component.$$;
+        if ($$.fragment !== null) {
+            run_all($$.on_destroy);
+            $$.fragment && $$.fragment.d(detaching);
+            // TODO null out other refs, including component.$$ (but need to
+            // preserve final state?)
+            $$.on_destroy = $$.fragment = null;
+            $$.ctx = [];
+        }
+    }
+    function make_dirty(component, i) {
+        if (component.$$.dirty[0] === -1) {
+            dirty_components.push(component);
+            schedule_update();
+            component.$$.dirty.fill(0);
+        }
+        component.$$.dirty[(i / 31) | 0] |= (1 << (i % 31));
+    }
+    function init(component, options, instance, create_fragment, not_equal, props, dirty = [-1]) {
+        const parent_component = current_component;
+        set_current_component(component);
+        const prop_values = options.props || {};
+        const $$ = component.$$ = {
+            fragment: null,
+            ctx: null,
+            // state
+            props,
+            update: noop,
+            not_equal,
+            bound: blank_object(),
+            // lifecycle
+            on_mount: [],
+            on_destroy: [],
+            before_update: [],
+            after_update: [],
+            context: new Map(parent_component ? parent_component.$$.context : []),
+            // everything else
+            callbacks: blank_object(),
+            dirty
+        };
+        let ready = false;
+        $$.ctx = instance
+            ? instance(component, prop_values, (i, ret, ...rest) => {
+                const value = rest.length ? rest[0] : ret;
+                if ($$.ctx && not_equal($$.ctx[i], $$.ctx[i] = value)) {
+                    if ($$.bound[i])
+                        $$.bound[i](value);
+                    if (ready)
+                        make_dirty(component, i);
+                }
+                return ret;
+            })
+            : [];
+        $$.update();
+        ready = true;
+        run_all($$.before_update);
+        // `false` as a special case of no DOM component
+        $$.fragment = create_fragment ? create_fragment($$.ctx) : false;
+        if (options.target) {
+            if (options.hydrate) {
+                const nodes = children(options.target);
+                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                $$.fragment && $$.fragment.l(nodes);
+                nodes.forEach(detach);
+            }
+            else {
+                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                $$.fragment && $$.fragment.c();
+            }
+            if (options.intro)
+                transition_in(component.$$.fragment);
+            mount_component(component, options.target, options.anchor);
+            flush();
+        }
+        set_current_component(parent_component);
+    }
+    class SvelteComponent {
+        $destroy() {
+            destroy_component(this, 1);
+            this.$destroy = noop;
+        }
+        $on(type, callback) {
+            const callbacks = (this.$$.callbacks[type] || (this.$$.callbacks[type] = []));
+            callbacks.push(callback);
+            return () => {
+                const index = callbacks.indexOf(callback);
+                if (index !== -1)
+                    callbacks.splice(index, 1);
+            };
+        }
+        $set() {
+            // overridden by instance, if it has props
+        }
+    }
+
+    /* src/HowTO.svelte generated by Svelte v3.20.1 */
+
+    function create_fragment(ctx) {
+    	let div;
+
+    	return {
+    		c() {
+    			div = element("div");
+    			div.innerHTML = `<img bp="offset-5@md 4@md 12@sm" src="how_to_handwash.gif" alt="hand washing" class="svelte-1p471kh">`;
+    			attr(div, "bp", "grid");
+    		},
+    		m(target, anchor) {
+    			insert(target, div, anchor);
+    		},
+    		p: noop,
+    		i: noop,
+    		o: noop,
+    		d(detaching) {
+    			if (detaching) detach(div);
+    		}
+    	};
+    }
+
+    class HowTO extends SvelteComponent {
+    	constructor(options) {
+    		super();
+    		init(this, options, null, create_fragment, safe_not_equal, {});
+    	}
+    }
+
+    /* src/ProgressBar.svelte generated by Svelte v3.20.1 */
+
+    function create_fragment$1(ctx) {
+    	let div2;
+    	let div1;
+    	let div0;
+    	let span;
+    	let t0;
+    	let t1;
+
+    	return {
+    		c() {
+    			div2 = element("div");
+    			div1 = element("div");
+    			div0 = element("div");
+    			span = element("span");
+    			t0 = text(/*progress*/ ctx[0]);
+    			t1 = text("% Complete");
+    			attr(span, "class", "sr-only svelte-g87qrr");
+    			attr(div0, "class", "progress-bar svelte-g87qrr");
+    			attr(div0, "role", "progressbar");
+    			set_style(div0, "width", /*progress*/ ctx[0] + "%");
+    			attr(div1, "class", "progress-container svelte-g87qrr");
+    			attr(div1, "bp", "offset-5@md 4@md 12@sm");
+    			attr(div2, "bp", "grid");
+    		},
+    		m(target, anchor) {
+    			insert(target, div2, anchor);
+    			append(div2, div1);
+    			append(div1, div0);
+    			append(div0, span);
+    			append(span, t0);
+    			append(span, t1);
+    		},
+    		p(ctx, [dirty]) {
+    			if (dirty & /*progress*/ 1) set_data(t0, /*progress*/ ctx[0]);
+
+    			if (dirty & /*progress*/ 1) {
+    				set_style(div0, "width", /*progress*/ ctx[0] + "%");
+    			}
+    		},
+    		i: noop,
+    		o: noop,
+    		d(detaching) {
+    			if (detaching) detach(div2);
+    		}
+    	};
+    }
+
+    function instance($$self, $$props, $$invalidate) {
+    	let { progress = 20 } = $$props;
+
+    	$$self.$set = $$props => {
+    		if ("progress" in $$props) $$invalidate(0, progress = $$props.progress);
+    	};
+
+    	return [progress];
+    }
+
+    class ProgressBar extends SvelteComponent {
+    	constructor(options) {
+    		super();
+    		init(this, options, instance, create_fragment$1, safe_not_equal, { progress: 0 });
+    	}
+    }
+
+    /* src/Timer.svelte generated by Svelte v3.20.1 */
+
+    function create_fragment$2(ctx) {
+    	let div0;
+    	let h2;
+    	let t0;
+    	let t1;
+    	let t2;
+    	let t3;
+    	let div1;
+    	let button;
+    	let t4;
+    	let current;
+    	let dispose;
+    	const progressbar = new ProgressBar({ props: { progress: /*progress*/ ctx[2] } });
+
+    	return {
+    		c() {
+    			div0 = element("div");
+    			h2 = element("h2");
+    			t0 = text("Seconds Left ");
+    			t1 = text(/*secondsLeft*/ ctx[0]);
+    			t2 = space();
+    			create_component(progressbar.$$.fragment);
+    			t3 = space();
+    			div1 = element("div");
+    			button = element("button");
+    			t4 = text("Start");
+    			attr(h2, "bp", "offset-5@md 4@md 12@sm");
+    			attr(h2, "class", "svelte-7drvi7");
+    			attr(div0, "bp", "grid");
+    			attr(button, "bp", "offset-5@md 4@md 12@sm");
+    			button.disabled = /*isRunning*/ ctx[1];
+    			attr(button, "class", "start svelte-7drvi7");
+    			attr(div1, "bp", "grid");
+    		},
+    		m(target, anchor, remount) {
+    			insert(target, div0, anchor);
+    			append(div0, h2);
+    			append(h2, t0);
+    			append(h2, t1);
+    			insert(target, t2, anchor);
+    			mount_component(progressbar, target, anchor);
+    			insert(target, t3, anchor);
+    			insert(target, div1, anchor);
+    			append(div1, button);
+    			append(button, t4);
+    			current = true;
+    			if (remount) dispose();
+    			dispose = listen(button, "click", /*startTimer*/ ctx[3]);
+    		},
+    		p(ctx, [dirty]) {
+    			if (!current || dirty & /*secondsLeft*/ 1) set_data(t1, /*secondsLeft*/ ctx[0]);
+    			const progressbar_changes = {};
+    			if (dirty & /*progress*/ 4) progressbar_changes.progress = /*progress*/ ctx[2];
+    			progressbar.$set(progressbar_changes);
+
+    			if (!current || dirty & /*isRunning*/ 2) {
+    				button.disabled = /*isRunning*/ ctx[1];
+    			}
+    		},
+    		i(local) {
+    			if (current) return;
+    			transition_in(progressbar.$$.fragment, local);
+    			current = true;
+    		},
+    		o(local) {
+    			transition_out(progressbar.$$.fragment, local);
+    			current = false;
+    		},
+    		d(detaching) {
+    			if (detaching) detach(div0);
+    			if (detaching) detach(t2);
+    			destroy_component(progressbar, detaching);
+    			if (detaching) detach(t3);
+    			if (detaching) detach(div1);
+    			dispose();
+    		}
+    	};
+    }
+
+    function instance$1($$self, $$props, $$invalidate) {
+    	const dispatch = createEventDispatcher();
+    	let secondsLeft = 20;
+    	let isRunning = false;
+    	let timer;
+
+    	function startTimer() {
+    		$$invalidate(1, isRunning = true);
+
+    		timer = setInterval(
+    			() => {
+    				$$invalidate(0, secondsLeft -= 1);
+
+    				if (secondsLeft <= 0) {
+    					clearInterval(timer);
+    					$$invalidate(0, secondsLeft = 20);
+    					$$invalidate(1, isRunning = false);
+    					dispatch("play", true);
+    				}
+    			},
+    			1000
+    		);
+    	}
+
+    	let progress;
+
+    	$$self.$$.update = () => {
+    		if ($$self.$$.dirty & /*secondsLeft*/ 1) {
+    			 $$invalidate(2, progress = (20 - secondsLeft) / 20 * 100);
+    		}
+    	};
+
+    	return [secondsLeft, isRunning, progress, startTimer];
+    }
+
+    class Timer extends SvelteComponent {
+    	constructor(options) {
+    		super();
+    		init(this, options, instance$1, create_fragment$2, safe_not_equal, {});
+    	}
+    }
+
+    /* src/App.svelte generated by Svelte v3.20.1 */
+
+    function create_fragment$3(ctx) {
+    	let div0;
+    	let t1;
+    	let t2;
+    	let t3;
+    	let div1;
+    	let t7;
+    	let audio;
+    	let current;
+    	const timer = new Timer({});
+    	timer.$on("play", /*play*/ ctx[1]);
+    	const howto = new HowTO({});
+
+    	return {
+    		c() {
+    			div0 = element("div");
+    			div0.innerHTML = `<h1 bp="12" class="svelte-1wdv9cp">Hand Washing App</h1>`;
+    			t1 = space();
+    			create_component(timer.$$.fragment);
+    			t2 = space();
+    			create_component(howto.$$.fragment);
+    			t3 = space();
+    			div1 = element("div");
+
+    			div1.innerHTML = `<h3 bp="offset-5@md 4@md 12@sm"><a href="https://www.who.int/gpsc/clean_hands_protection/en/">
+      Picture Source
+    </a> 
+    <a href="https://freesound.org/people/metrostock99/sounds/345086/">
+      Sound Attribution
+    </a></h3>`;
+
+    			t7 = space();
+    			audio = element("audio");
+    			audio.innerHTML = `<source src="sound.wav" type="audio/wav">`;
+    			attr(div0, "bp", "grid");
+    			attr(div1, "bp", "grid");
+    		},
+    		m(target, anchor) {
+    			insert(target, div0, anchor);
+    			insert(target, t1, anchor);
+    			mount_component(timer, target, anchor);
+    			insert(target, t2, anchor);
+    			mount_component(howto, target, anchor);
+    			insert(target, t3, anchor);
+    			insert(target, div1, anchor);
+    			insert(target, t7, anchor);
+    			insert(target, audio, anchor);
+    			/*audio_binding*/ ctx[2](audio);
+    			current = true;
+    		},
+    		p: noop,
+    		i(local) {
+    			if (current) return;
+    			transition_in(timer.$$.fragment, local);
+    			transition_in(howto.$$.fragment, local);
+    			current = true;
+    		},
+    		o(local) {
+    			transition_out(timer.$$.fragment, local);
+    			transition_out(howto.$$.fragment, local);
+    			current = false;
+    		},
+    		d(detaching) {
+    			if (detaching) detach(div0);
+    			if (detaching) detach(t1);
+    			destroy_component(timer, detaching);
+    			if (detaching) detach(t2);
+    			destroy_component(howto, detaching);
+    			if (detaching) detach(t3);
+    			if (detaching) detach(div1);
+    			if (detaching) detach(t7);
+    			if (detaching) detach(audio);
+    			/*audio_binding*/ ctx[2](null);
+    		}
+    	};
+    }
+
+    function instance$2($$self, $$props, $$invalidate) {
+    	let sound;
+
+    	function play() {
+    		sound.play();
+    	}
+
+    	function audio_binding($$value) {
+    		binding_callbacks[$$value ? "unshift" : "push"](() => {
+    			$$invalidate(0, sound = $$value);
+    		});
+    	}
+
+    	return [sound, play, audio_binding];
+    }
+
+    class App extends SvelteComponent {
+    	constructor(options) {
+    		super();
+    		init(this, options, instance$2, create_fragment$3, safe_not_equal, {});
+    	}
+    }
+
+    const app = new App({
+      target: document.body
+    });
+
+    return app;
+
+}());
+//# sourceMappingURL=bundle.js.map
